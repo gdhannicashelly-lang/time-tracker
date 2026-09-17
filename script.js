433b5fe32fcef1430ed2d3dc0d5ae1c23ba096c6
gdhannicashelly-lang <gdhannicashelly@gmail.com>
2026-09-17 23:05:47 +0800

    script.js

--- script.js
+++ script.js
@@ -46,26 +46,13 @@ import {
 */
 
 const firebaseConfig = {
-
-    apiKey: "YOUR_API_KEY",
-
-    authDomain:
-        "YOUR_PROJECT_ID.firebaseapp.com",
-
-    projectId:
-        "YOUR_PROJECT_ID",
-
-    storageBucket:
-        "YOUR_PROJECT_ID.firebasestorage.app",
-
-    messagingSenderId:
-        "YOUR_MESSAGING_SENDER_ID",
-
-    appId:
-        "YOUR_APP_ID"
-
+  apiKey: "AIza...",
+  authDomain: "your-project.firebaseapp.com",
+  projectId: "your-project",
+  storageBucket: "your-project.firebasestorage.app",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
 };
-
 
 /* =========================================================
    FIREBASE INITIALIZATION
